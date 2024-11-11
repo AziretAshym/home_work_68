eb48c79d16331485e360df902eb4ee4cf4e0d6cb
Ashymov Aziret <aziretashym@gmail.com>
2024-11-11 17:46:10 +0600

    Added loaders for requests.

---
+++ src/UI/ButtonSpinner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ButtonSpinner = () => {
+  return (
+    <button className="btn bg-primary-subtle" type="button" disabled>
+      <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>
+      <span role="status">Loading...</span>
+    </button>
+  );
+};
+
+export default ButtonSpinner;
--- src/containers/TodoList/TodoList.tsx
+++ src/containers/TodoList/TodoList.tsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch } from '../../api/store';
+import { AppDispatch, RootState } from '../../api/store';
 import { selectTodos, fetchList, addTodo, switchTodo, deleteTodo } from './todoListSlice';
+import Spinner from '../../UI/Spinner.tsx';
+import ButtonSpinner from '../../UI/ButtonSpinner.tsx';
 
 const TodosList: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const todos = useSelector(selectTodos);
   const [text, setText] = useState('');
+  const [adding, setAdding] = useState(false);
+  const [deletingId, setDeletingId] = useState<string | null>(null);
+  const [switchingId, setSwitchingId] = useState<string | null>(null);
+  const loading = useSelector((state: RootState) => state.todos.loading);
 
 
   useEffect(() => {
@@ -16,18 +22,24 @@ const TodosList: React.FC = () => {
 
   const onAddTodo = () => {
     if (text.trim()) {
+      setAdding(true);
       dispatch(addTodo(text));
       setText('');
+      setAdding(false);
     }
   };
 
 
   const onSwitchTodo = (id: string) => {
+    setSwitchingId(id)
     dispatch(switchTodo(id));
+    setSwitchingId(null);
   };
 
   const onDeleteTodo = (id: string) => {
+    setDeletingId(id);
     dispatch(deleteTodo(id));
+    setDeletingId(null);
   };
 
   return (
@@ -42,37 +54,49 @@ const TodosList: React.FC = () => {
               placeholder="Enter new task"
               value={text}
               onChange={(e) => setText(e.target.value)}
+              disabled={adding}
             />
-            <button className="btn btn-primary" onClick={onAddTodo}>
-              Add Task
+            <button className="btn btn-primary" onClick={onAddTodo} disabled={adding}>
+              {adding ? <ButtonSpinner /> : 'Add Task'}
             </button>
           </div>
 
-
+          {loading ? (
+            <div className="text-center z-3">
+              <Spinner />
+            </div>
+          ) : (
             <ul className="list-group w-50 mx-auto">
-            {todos.slice().reverse().map((todo) => (
-              <li key={todo.id}
-                  className={`list-group-item mb-3 d-flex align-items-center justify-content-between  
+              {todos.slice().reverse().map((todo) => (
+                <li key={todo.id}
+                    className={`list-group-item mb-3 d-flex align-items-center justify-content-between  
                   ${todo.done ? 'bg-primary-subtle text-primary' : ''}`}>
                 <span>
                   {todo.text}
                 </span>
 
-                <div className="d-flex flex-column align-items-center gap-2">
-                  <input
-                    type="checkbox"
-                    checked={todo.done}
-                    onChange={() => onSwitchTodo(todo.id)}
-                    className="form-check-input me-2"
-                  />
+                  <div className="d-flex flex-column align-items-center gap-2">
+                    <input
+                      type="checkbox"
+                      checked={todo.done}
+                      onChange={() => onSwitchTodo(todo.id)}
+                      className="form-check-input me-2"
+                      disabled={switchingId === todo.id}
+                    />
 
-                  <button className="btn btn-danger btn-sm" onClick={() => onDeleteTodo(todo.id)}>
-                    Delete
-                  </button>
-                </div>
-              </li>
-            ))}
+                    <button className="btn btn-danger btn-sm"
+                            onClick={() => onDeleteTodo(todo.id)}
+                            disabled={deletingId === todo.id}
+                    >
+                      {deletingId === todo.id ? <ButtonSpinner /> : 'Delete'}
+                    </button>
+                  </div>
+                </li>
+              ))}
             </ul>
+          )
+          }
+
         </div>
       </div>
     </div>
